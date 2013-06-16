db8d8feb1cb34a54b01d9deff48450100c16fdc7
Nicolas Gramlich <ngramlich@zynga.com>
2013-06-15 21:44:41 -0700

    Added Actions for open and delete.

--- src/org/andengine/extension/server/gcm/util/constants/GCMConstants.java
+++ src/org/andengine/extension/server/gcm/util/constants/GCMConstants.java
@@ -14,6 +14,10 @@ public interface GCMConstants extends ServerConstants {
 	// ===========================================================
 
 	public static final String ACTION_GCM_MESSAGE = "org.andengine.extension.server.gcm.ACTION_GCM_MESSAGE";
+	public static final String ACTION_GCM_MESSAGE_OPEN = "org.andengine.extension.server.gcm.ACTION_GCM_MESSAGE_OPEN";
+	public static final String ACTION_GCM_MESSAGE_DELETE = "org.andengine.extension.server.gcm.ACTION_GCM_MESSAGE_DELETE";
+	public static final String ACTION_GCM_MESSAGE_OPEN_EXTRA_ID = "id";
+	public static final String ACTION_GCM_MESSAGE_DELETE_EXTRA_ID = "id";
 
 	// ===========================================================
 	// Methods
